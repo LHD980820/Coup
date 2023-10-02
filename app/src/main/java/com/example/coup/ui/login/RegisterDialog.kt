75f646f9d06790c9e0f2d876e11166aa52f521f0
장락영 <jfkrdud@gmail.com>
2023-10-03 01:35:04 +0900

    login2register2

---
+++ app/src/main/java/com/example/coup/ui/login/RegisterDialog.kt
@@ -0,0 +1,16 @@
+package com.example.coup.ui.login
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.view.Window
+import com.example.coup.R
+
+class RegisterDialog(context: Context) : Dialog(context) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.dialog_register)
+    }
+}
