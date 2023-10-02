15080d6ea57cdcba8ae34b9a673aa31efbf9ddd7
LHD980820 <dukytogtog@gmail.com>
2023-10-03 05:32:55 +0900

    homepage

--- app/src/main/java/com/example/coup/HomeActivity.kt
+++ app/src/main/java/com/example/coup/HomeActivity.kt
@@ -1,12 +1,40 @@
 package com.example.coup
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.navigation.NavigationBarView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import android.view.MenuItem
 
-class HomeActivity : Activity() {
+
+private const val TAG_LIST = "list_fragment"
+private const val TAG_RANKING = "ranking_fragment"
+private const val TAG_INFO = "info_fragment"
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: NavigationBarView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        supportFragmentManager.beginTransaction().add(R.id.home_frame, room_list()).commit()
+        bottomNavigationView.setOnItemSelectedListener { item->
+            when(item.itemId) {
+                R.id.icon_room -> setFragment(room_list())
+                R.id.icon_ranking -> setFragment(ranking())
+                R.id.icon_user_info -> setFragment(info())
+                else -> setFragment(room_list())
+            }
+            true
+        }
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        val manager: FragmentManager = supportFragmentManager
+        val fragTransaction = manager.beginTransaction()
+        fragTransaction.replace(R.id.home_frame, fragment).commit()
     }
 }
