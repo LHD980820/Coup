a4a806ee0f2e234a0a582a89c69a55624f674e13
LHD980820 <dukytogtog@gmail.com>
2023-10-03 22:05:37 +0900

    implement log-in function

--- app/src/main/java/com/example/coup/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/coup/ui/login/LoginActivity.kt
@@ -3,12 +3,18 @@ package com.example.coup.ui.login
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.example.coup.HomeActivity
 import com.example.coup.R
 import com.google.android.gms.common.SignInButton
+import com.google.firebase.auth.FirebaseAuth    //밑에 전부 다 파이어베이스
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : Activity() {
     // UI references.
@@ -16,10 +22,16 @@ class LoginActivity : Activity() {
     private lateinit var mPasswordView: EditText
     private lateinit var mGoogleLogin: SignInButton
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+        // [END initialize_auth]
 
         // Set up the login form.
         mEmailView = findViewById(R.id.editview_email)
@@ -43,15 +55,15 @@ class LoginActivity : Activity() {
         }
 
         // Buttons
-        val mEmailSignInButton = findViewById<Button>(R.id.email_log_in_button)
+        val mEmailLogInButton = findViewById<Button>(R.id.email_log_in_button)
         val mEmailSignUpButton = findViewById<Button>(R.id.email_sign_up_button)
         val forgotPasswordButton = findViewById<Button>(R.id.password_forgot)
 
         // event handler
-        mEmailSignInButton.setOnClickListener {
-            val intent = Intent(applicationContext, HomeActivity::class.java)
-            startActivity(intent)
-            finish()
+        mEmailLogInButton.setOnClickListener {
+            val id: String = mEmailView.text.toString()
+            val pw: String = mPasswordView.text.toString()
+            signIn(id, pw)
         }
 
         // sign-up button event handler
@@ -64,5 +76,76 @@ class LoginActivity : Activity() {
             val intent = Intent(applicationContext, LoginResetPassword::class.java)
             startActivity(intent)
         }
+    }
+
+    // [START on_start_check_user]
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            reload()
+        }
+    }
+    // [END on_start_check_user]
+
+
+
+    //로그인
+    private fun signIn(email: String, password: String) {
+        if(email == null) {
+            Toast.makeText(baseContext, "이메일을 입력하세요", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if(password == null) {
+            Toast.makeText(baseContext, "비밀번호를 입력하세요", Toast.LENGTH_SHORT).show()
+            return
+        }
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+
+                    Toast.makeText(baseContext, "$email 님 환영합니다!", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(applicationContext, HomeActivity::class.java)
+                    intent.putExtra("userinfo", user)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    updateUI(null)
+                }
+            }
+        // [END sign_in_with_email]
+    }
+
+    private fun sendEmailVerification() {
+        // [START send_email_verification]
+        val user = auth.currentUser!!
+        user.sendEmailVerification()
+            .addOnCompleteListener(this) { task ->
+                // Email Verification sent
+            }
+        // [END send_email_verification]
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+    }
+
+    private fun reload() {
+    }
+
+    companion object {
+        private const val TAG = "EmailPassword"
     }
 }
--- app/src/main/java/com/example/coup/ui/login/RegisterActivity.kt
+++ app/src/main/java/com/example/coup/ui/login/RegisterActivity.kt
@@ -2,13 +2,20 @@ package com.example.coup.ui.login
 
 import android.app.Activity
 import android.content.Intent
+import android.nfc.Tag
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.example.coup.R
+import com.google.firebase.auth.FirebaseAuth    //밑에 전부 다 파이어베이스
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : Activity(){
     // UI references.
@@ -16,9 +23,16 @@ class RegisterActivity : Activity(){
     private lateinit var mPasswordView: EditText
     private lateinit var mPasswordCheckView: EditText
     private lateinit var mEmailSignUpButton: Button
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+        // [END initialize_auth]
 
         // Set up the register form.
         mEmailView = findViewById(R.id.editview_email2)
@@ -51,18 +65,69 @@ class RegisterActivity : Activity(){
 
         // sign-up button event handler
         mEmailSignUpButton.setOnClickListener {
-            val dialog = RegisterDialog(this)
-            dialog.show()
+            val id: String = mEmailView.text.toString()
+            val pw: String = mPasswordView.text.toString()
+            val pwc: String = mPasswordCheckView.text.toString()
+            if(id == null) {
+                Toast.makeText(baseContext, "이메일을 입력하세요", Toast.LENGTH_SHORT).show()
+            }
+            if(pw == null) {
+                Toast.makeText(baseContext, "비밀번호를 입력하세요", Toast.LENGTH_SHORT).show()
+            }
+            if(pwc == null) {
+                Toast.makeText(baseContext, "밑에 칸에 비밀번호를 한번 더 입력하세요", Toast.LENGTH_SHORT).show()
+            }
+            if(pw != pwc) {
+                Toast.makeText(baseContext, "비밀번호가 맞지 않습니다 ($pw + $pwc)", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                createAccount(id, pw)
 
-            Handler(Looper.getMainLooper()).postDelayed({
-
-                // 일정 시간이 지나면 LoginActivity로 이동
-                val intent= Intent( this, LoginActivity::class.java)
-                startActivity(intent)
-
-                finish()
-
-            }, 3000)
+            }
         }
+    }
+
+    //계정 생성
+    private fun createAccount(email: String, password: String) {
+
+        // [START create_user_with_email]
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+
+                    val dialog = RegisterDialog(this)
+                    dialog.show()
+
+                    Handler(Looper.getMainLooper()).postDelayed({
+
+                        // 일정 시간이 지나면 LoginActivity로 이동
+                        val intent= Intent( this, LoginActivity::class.java)
+                        startActivity(intent)
+                        finish()
+
+                    }, 1000)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(RegisterActivity.TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    updateUI(null)
+                }
+            }
+        // [END create_user_with_email]
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+    }
+
+    companion object {
+        private const val TAG = "EmailPassword"
     }
 }
