4092fc5ba6d91bf9bc30a1a22bda48b5e7125405
장락영 <jfkrdud@gmail.com>
2023-10-04 01:26:49 +0900

    WaitingRoomDesign

---
+++ app/src/main/java/com/example/coup/GameWaitingRoomActivity.kt
@@ -0,0 +1,13 @@
+package com.example.coup
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GameWaitingRoomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_waiting_room)
+
+
+    }
+}
