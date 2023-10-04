c676b20e5826030aa085d8c1e5b64608161c6637
coyspursss <qkrwognl1703@naver.com>
2023-10-04 18:21:19 +0900

    대기실에서 로비로 나가기 추가

--- app/src/main/java/com/example/coup/GameWaitingRoomActivity.kt
+++ app/src/main/java/com/example/coup/GameWaitingRoomActivity.kt
@@ -1,14 +1,17 @@
 package com.example.coup
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class GameWaitingRoomActivity : AppCompatActivity() {
     //UI Preferences
+    private lateinit var mOutButton: ImageButton
     private lateinit var mGameStartButton: Button
     private lateinit var mOptionButton: Button
 
@@ -16,7 +19,19 @@ class GameWaitingRoomActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_waiting_room)
 
+        mOutButton = findViewById(R.id.back_lobby)
+        mGameStartButton = findViewById(R.id.game_start_btn)
         mOptionButton = findViewById(R.id.option_btn)
+
+        mOutButton.setOnClickListener {
+            val intent = Intent(applicationContext,HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        mGameStartButton.setOnClickListener {
+            // TODO: Game Start
+        }
+
         mOptionButton.setOnClickListener {
             val dialog = RoomOptionDialog(this)
             dialog.show()
